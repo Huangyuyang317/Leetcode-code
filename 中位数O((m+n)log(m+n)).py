ad2912f06c03eea440139b24f0878a4bbba604d5
Huangyuyang317 <106917354+Huangyuyang317@users.noreply.github.com>
2025-09-07 23:02:50 +0800

    Delete 中位数O((m+n)log(m+n)).py

--- 中位数O((m+n)log(m+n)).py
+++
@@ -1,7 +0,0 @@
-nums=nums1+nums2
-nums.sort(reverse=True)
-if nums%2:
-    print(nums[len((nums-1)/2)])
-else:
-    print((nums[len((nums-1)/2)]+nums[len((nums+1)/2)])/2)
-# 这个算法的时间复杂度为O((m+n)log(m+n)),这是排序的复杂度，合并的复杂度为O(m+n),所以最大的是O((m+n)log(m+n))
